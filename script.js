3ef298f85910f8b9507fdaa5f72198732fac6916
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-07-19 00:32:50 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle('show', entry.isIntersecting)
+        })
+    },
+    {
+        threshold: 0.5
+    }
+)
+
+const cards = document.querySelectorAll('.card')
+cards.forEach(card => {
+    observer.observe(card)
+})
