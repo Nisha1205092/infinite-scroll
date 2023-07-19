b6a45baeb087e3ac5625da842b5cc8020b05f15a
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-07-19 17:17:53 +0600

    done with the infinite scroll effect, rootMargin not working

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+const cardContainer = document.querySelector('.card-container');
+
+const loadNewCards = () => {
+    for (let i = 0; i < 10; i++) {
+        let card = document.createElement('div')
+        card.textContent = 'New Card'
+        card.classList.add(['card'])
+        observer.observe(card)
+        cardContainer.append(card)
+    }
+}
+
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach(entry => {
+            console.log(entry)
             entry.target.classList.toggle('show', entry.isIntersecting)
+
+            /**
+             * to animate something only the first time
+             * in case of lazy loading something (e.g., an image)
+             * we can load it the first time and unobserve it.
+             * 
+            if (entry.isIntersecting) {
+                observer.unobserve(entry.target)
+            } */
+
         })
     },
     {
@@ -9,7 +32,23 @@ const observer = new IntersectionObserver(
     }
 )
 
+const lastCardObserver = new IntersectionObserver(entries => {
+    const lastCard = entries[0];
+    if (!lastCard.isIntersecting) {
+        return
+    }
+    loadNewCards()
+    // unobserve the original last card
+    lastCardObserver.unobserve(lastCard.target)
+    // observe the "new last card" every time, essentially an infinite scroll
+    // useful for cases like social media apps or youtube like platforms, 
+    // where they keep loading content as a user scrolls down and down
+    lastCardObserver.observe(document.querySelector('.card:last-child'))
+}, {})
+
 const cards = document.querySelectorAll('.card')
 cards.forEach(card => {
     observer.observe(card)
 })
+
+lastCardObserver.observe(document.querySelector('.card:last-child'))
